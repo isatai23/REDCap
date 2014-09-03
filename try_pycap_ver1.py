139ae2f0a08c3eaf2847d1c1755e49b571dcc2da
Isabella Murphy <isabella.murphy@gmail.com>
2014-09-03 16:30:50 -0400

    Add export fields of interest code on try_pycap_ver1.py

--- try_pycap_ver1.py
+++ try_pycap_ver1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+
 # Filename: try_pycap_ver1.py
 
 # Import PyCap module
@@ -9,41 +9,43 @@ from redcap import Project, RedcapError
 
 URL = 'https://poa-redcap.med.yale.edu/api/'
 TOKEN = '0BD89F3E896E72920DF3CFAC8DD739D7'
-
 TOKEN2 = 'CAF3713EF2307C80BAD789DBAFBAA8C7'
 # Call PyCap Project function
 
-project = Project(URL, TOKEN)
-project2 = Project(URL, TOKEN2)
-#project2 = Project(URL, TOKEN2)
+family = Project(URL, TOKEN)
+srs = Project(URL, TOKEN2)
+
 
 # Get the metatdata of the project
 
-metadata = project.export_metadata()
-metadata2 = project2.export_metadata()
-#metatdata2 = project2.export_metadata()
-
-for field in metadata:
-	print "%s (%s) ---> %s" % (field['field_name'], field['field_type'], field['field_label'])
+#metadata = family.export_metadata()
+#metadata2 = srs.export_metadata()
 
 
-#project2 = Project(URL, TOKEN2)
-#metadata2 = project2.export_metadata()
-for field in metadata2:
-	print "%s (%s) ---> %s" % (field['field_name'], field['field_type'], field['field_label'])
+#for field in metadata:
+#	print "%s (%s) ---> %s" % (field['field_name'], field['field_type'], field['field_label'])
+
+
+
+#for field in metadata2:
+#	print "%s (%s) ---> %s" % (field['field_name'], field['field_type'], field['field_label'])
 
 # Get the data of the project
 
-data = project.export_records()
-data2 = project2.export_records()
-print "The first project has %d records" % len(data)
-print "The second project has %d records" % len(data2)
-#print "Each record has the following keys: %s." % ', '.join(data[0].keys())
+family_fields_intrst = ['individual_id', 'sex']
+srs_fields_intrst = ['individual_id', 'srs2_tscore']
+
+familyData = family.export_records(fields = family_fields_intrst)
+srsData = srs.export_records(fields = srs_fields_intrst)
+print "The first project has %d records" % len(familyData)
+print "The second project has %d records" % len(srsData)
 
 
-print
 
-#print "But our metadata structure has the following fields: %s!" % ','.join(field['field_name'] for field in metadata)
-print
+print familyData[0]
+print srsData[0]
+
+#iterate the records
+
 
 
